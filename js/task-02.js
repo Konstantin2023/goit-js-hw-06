f44d911fc71ee76259f804c57a27595e63b33c0a
Konstantin2023 <pscosx@gmail.com>
2022-12-11 15:32:32 +0200

    1 і 2 завдання.

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,31 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const body = document.body;
+const pGoBack = body.firstElementChild;
+const ulIngredients = pGoBack.nextElementSibling;
+const Li1 = document.createElement("Li");
+Li1.classList.add("item");
+Li1.textContent = ingredients[0];
+const Li2 = document.createElement("Li");
+Li2.classList.add("item");
+Li2.textContent = ingredients[1];
+const Li3 = document.createElement("Li");
+Li3.classList.add("item");
+Li3.textContent = ingredients[2];
+const Li4 = document.createElement("Li");
+Li4.classList.add("item");
+Li4.textContent = ingredients[3];
+const Li5 = document.createElement("Li");
+Li5.classList.add("item");
+Li5.textContent = ingredients[4];
+const Li6 = document.createElement("Li");
+Li6.classList.add("item");
+Li6.textContent = ingredients[5];
+
+ulIngredients.after(Li1, Li2, Li3, Li4, Li5, Li6);
