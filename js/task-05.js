cd1a7c6e8883602bb9d2874d848d1d8be6eb35e7
Konstantin2023 <pscosx@gmail.com>
2022-12-19 21:46:35 +0200

    Моя дз.

--- js/task-05.js
+++ js/task-05.js
@@ -1,15 +1,6 @@
 const inputText = document.querySelector("#name-input");
 const spanText = document.querySelector("#name-output");
 
-// inputText.addEventListener("input", onInput);
-
-// function onInput(event) {
-//   spanText.textContent = event.currentTarget.value;
-//   if (spanText.value === "") {
-//     return (spanText.textContent = "Anonymous");
-//   }
-// }
-
 inputText.addEventListener("input", (event) => {
   spanText.textContent = event.currentTarget.value;
 });
